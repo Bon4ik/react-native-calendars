c9faf06b217b11a609785e777a110ada65bc3eba
AppliKeySolutions <applikeytester@gmail.com>
2017-07-05 19:39:24 +0300

    Update index.js

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -47,7 +47,7 @@ class CalendarHeader extends Component {
 
   renderExercisePanel = () => {
     const currentData = new Date();
-    const currentDay = currentData.getDay();
+    const currentDay = currentData.getDate();
     // const days = [1, 2, 3, 4, 5, 6, 7];
     const days = [
       {regular: IC_REST, accent: IC_REST_ACCENT, id: 7}, // 0
